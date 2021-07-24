b93b1db20ea3e94df640cfda0d993403e82a8830
Blebot0 <keshav29kapur@gmail.com>
2021-07-24 19:21:00 +0530

    Updated Index Buffer with Error Handling and Uniform Shader Property

--- OpenGL_dev/Readme.md
+++ OpenGL_dev/Readme.md
@@ -3,4 +3,7 @@
 
 - OpenGL is an interface developed by GPU Manufacturers to use there GPU for rendering
 - This Repository has been setup for Windows 10 and VS 19 for 32-bit Architecture
-	
+
+-----------------
+
+TODO: Add CmakeLists to the repo
--- OpenGL_dev/src/IndexBuffer.cpp
+++ OpenGL_dev/src/IndexBuffer.cpp
@@ -1,11 +1,33 @@
 #include <glew.h>
 #include <GLFW/glfw3.h>
-
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <sstream>
 
+// Debug Break is a function provided by MS VS
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+/***************** Function to Catch errors in OpenGL ***************/
+
+static void GLClearError() {
+        
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+
+    if (GLenum error = glGetError()) {
+
+        std::cout << "[OpenGL Error] Error Code: ( " << error << " ) "<<function<<" "<<file<<" "<<line<<std::endl;
+        return false;
+    }
+
+    return true;
+}
 /***************** Structure to define string of Vertex and Fragment Shaders ***************/
 struct ShaderProgramSource
 {
@@ -40,14 +62,14 @@ static ShaderProgramSource ParseShader(const std::string& filepath) {
             }
         }
 
-        else{
+        else {
             ss[(int)type] << line << '\n';
         }
     }
     return { ss[0].str(), ss[1].str() };
 }
 /********************* Function to Compile Shaders ******************************/
-static unsigned int CompileShader( unsigned int type, const std::string& source) {
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
 
     unsigned int id = glCreateShader(type);
     const char* src = source.c_str();
@@ -62,7 +84,7 @@ static unsigned int CompileShader( unsigned int type, const std::string& source)
         std::cout << "FAILED TO COMPILE SHADER\n";
 
         glDeleteShader(id);
-        
+
         return 0;
     }
     return id;
@@ -88,7 +110,7 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 
 }
 
-int Shader(void)
+int IndexBuffer(void)
 {
     GLFWwindow* window;
 
@@ -97,7 +119,7 @@ int Shader(void)
         return -1;
 
     /***************** Create a windowed mode window and its OpenGL context **************************/
-    window = glfwCreateWindow(640, 480, "Purple Triangle", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Purple Square", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -112,14 +134,24 @@ int Shader(void)
         std::cout << "GLEW isn't imported correctly" << std::endl;
     }
 
+    // Increase the Interval between frames
+    glfwSwapInterval(1);
+
     /******************** Vertex definition *****************************/
-    float positions[6] = {
+    float positions[] = {
         // Each line is a vertex 
         // Here we are just using vertex position as the only attribute
         -0.5f, -0.5f,
-         0.f, 0.5f,
-         0.5f, -0.5f
+         0.5f, -0.5f,
+         0.5f,  0.5f,
+        -0.5f,  0.5f,
     };
+
+    unsigned int indices[] = {
+        0, 1, 2, // indices of Vertex of First Triangle
+        2, 3, 0
+    };
+
     unsigned int buffer;
 
     /************************* Allocating Buffer Space in GPU VRAM *****************/
@@ -127,36 +159,59 @@ int Shader(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
 
     /************************** Array Buffer with Float type Vertex Position *******************/
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
 
 
     /********************* This is the function used to determine the vertex Attribute *****************************/
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    unsigned int ibo; // index buffer object
 
 
+    /************************* Allocating Index Buffer Space in GPU VRAM *****************/
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+    /************************** Array Buffer with Float type Indices Position *******************/
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("resources/shaders/basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader); 
+    glUseProgram(shader);
     
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    
+    float r = 0.00f, g = 0.00f, b = 0.00f, increment = 0.05f;
+
     /***************** Loop until the user closes the window ****************/
     while (!glfwWindowShouldClose(window))
     {
         /**************** Render here *********************/
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /************************ Changning Colours in Runtime using Uniform ****************/
+        GLCall(glUniform4f(location, r, 0.0f, 0.0f, 1.0f));
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        if (r > 1.0f)
+            increment -= 0.05f;
+        else if (r < 0.0f) {
+            increment += 0.05f;
+        }
+
+        r += increment;
+
+        
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
         /********************* Swap front and back buffers *********************/
         glfwSwapBuffers(window);
 
         /********************** Poll for and process events ********************/
         glfwPollEvents();
     }
-    
+
     /********************** Deleting Shaders ********************/
     glDeleteProgram(shader);
     glfwTerminate();
